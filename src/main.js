1d227387ed0909f7243839b1884a0d305f9084e0
Zerofelx <zeroflex3@gmail.com>
2021-04-07 23:01:33 -0500

    Haciendo commit para el deploy

--- src/main.js
+++ src/main.js
@@ -20,6 +20,11 @@ Vue.use(VueSimpleAlert)
 
 Vue.config.productionTip = false
 
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
+});
+
 new Vue({
   router,
   render: h => h(App),
